3a829b8ec15e9117c3799740c389101ffafe80b1
jdcook <jaredc35@gmail.com>
2014-10-13 16:55:08 -0500

    finished backend for rating

--- CySwap/app/models/User.php
+++ CySwap/app/models/User.php
@@ -3,50 +3,30 @@
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User {
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
-
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password');
-
-	/**
-	 * Get the unique identifier for the user.
-	 *
-	 * @return mixed
-	 */
-	public function getAuthIdentifier()
-	{
-		return $this->getKey();
+	public function thumbsUp($username){
+		$user=DB::select("SELECT * from cyswap.feedback where user = ? limit 0,1", array($username));
+		if(count($user)){
+			$num = $user[0]->positive;
+			$num = $num + 1;
+			DB::update("UPDATE cyswap.feedback set positive = ? where user = ?", array($num, $username));
+		}
+		else{
+			DB::insert("INSERT into cyswap.feedback (user, positive, negative) VALUES (?,?,?)", array($username, 1, 0));
+		}
 	}
 
-	/**
-	 * Get the password for the user.
-	 *
-	 * @return string
-	 */
-	public function getAuthPassword()
-	{
-		return $this->password;
-	}
-
-	/**
-	 * Get the e-mail address where password reminders are sent.
-	 *
-	 * @return string
-	 */
-	public function getReminderEmail()
-	{
-		return $this->email;
+	public function thumbsDown($username){
+		$user=DB::select("SELECT * from cyswap.feedback where user = ? limit 0,1", array($username));
+		if(count($user)){
+			$num = $user[0]->negative;
+			$num = $num + 1;
+			DB::update("UPDATE cyswap.feedback set negative = ? where user = ?", array($num, $username));
+		}
+		else{
+			DB::insert("INSERT into cyswap.feedback (user, positive, negative) VALUES (?,?,?)", array($username, 1, 0));
+		}
 	}
 
 }
