a6c8469c5bb640ca9c42f91b451e5c95c1225b07
Adam Sunderman <amsunderman@gmail.com>
2014-12-06 12:25:32 -0600

    rideshare and amesrent links added
    added rideshare and amesrent buttons at the end of category list

--- CySwap/app/views/categories.blade.php
+++ CySwap/app/views/categories.blade.php
@@ -10,5 +10,11 @@
           <a class="btn btn-default" href="{{URL::to('category/'.$category)}}" role="button"><?php $category = ucfirst($category);?> {{$category}} »</a>
     </div>
 @endforeach
+<div class="col-md-4">
+          <a class="btn btn-default" href="http://www.rideshare.gsb.iastate.edu" role="button">Ride Share »</a>
+    </div>
+<div class="col-md-4">
+          <a class="btn btn-default" href="http://www.amesrent.com" role="button">Ames Rent »</a>
+    </div>
 </div>
 @stop
