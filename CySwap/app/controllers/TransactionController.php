92a23bb249ed099067fff0f4f32c3af4571b0b61
Adam Sunderman <amsunderman@gmail.com>
2014-12-08 16:27:25 -0600

    kyle is no longer cyswap
    updated transaction controller to send from cyswap@iastate.edu

--- CySwap/app/controllers/TransactionController.php
+++ CySwap/app/controllers/TransactionController.php
@@ -10,7 +10,7 @@ class TransactionController extends BaseController {
             {
                 $poster = Input::get('posterName');
                 $buyer = Session::get('user');
-                $message->sender('kabernsj@iastate.edu', 'CySwap')
+                $message->sender('cyswap@iastate.edu', 'CySwap')
                 ->to($poster.'@iastate.edu', $poster)
                 ->subject($buyer.' is interested in buying your item!')
                 ->replyTo($buyer.'@iastate.edu', $buyer);
@@ -33,7 +33,7 @@ class TransactionController extends BaseController {
                 Mail::send('emails.rate', array('postid'=>$postid, 'poster'=>$username), function($message)
                 {
                     $buyerName = Input::get('buyerName');
-                    $message->sender('kabernsj@iastate.edu', 'CySwap')
+                    $message->sender('cyswap@iastate.edu', 'CySwap')
                     ->to($buyerName.'@iastate.edu', $buyerName)
                     ->subject('Cyswap Transaction Completed');
                 });
@@ -41,7 +41,7 @@ class TransactionController extends BaseController {
             }
             else{
                 App::make('Post')->deletePost(Input::get('postid'));
-                return Redirect::to('/outputMessage')->with('message', 'The post has been deleted.'); 
+                return Redirect::to('/outputMessage')->with('message', 'The post has been deleted.');
             }
 
         }
