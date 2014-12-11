20e3f5a84a9d2c33df091db9880755dca58cba0c
Kyle Johnson <kabernsj@gmail.com>
2014-12-11 16:07:45 -0600

    Fixed Rate User

--- CySwap/app/views/emails/rate.php
+++ CySwap/app/views/emails/rate.php
@@ -5,7 +5,7 @@
 
 echo "<a href='".URL::to('rateBuyer/'.$poster.'/'.$postid)
 ."'>Click Here</a> to rate your interaction with ".$poster
-." for <a href='"URL::to('viewpost/'.$postid)."'>this item.</a>";
+." for <a href='".URL::to('viewpost/'.$postid)."'>this item.</a>";
 
 
 ?>
